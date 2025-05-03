f22f5a39e0a5009310009c2b08cf08246e2213da
Wolfario <elizovmaksim1@gmail.com>
2025-05-03 16:34:15 +0200

    Implement automated dataset download from kaggle

--- airflow/dags/etl_pipeline.py
+++ airflow/dags/etl_pipeline.py
@@ -3,8 +3,11 @@ from datetime import datetime
 import pandas as pd
 from sqlalchemy import create_engine
 import os
+import zipfile
 
-CSV_PATH = "/opt/airflow/data/games.csv"
+ZIP_PATH = "/opt/airflow/data/steam-games-dataset.zip"
+EXTRACT_PATH = "/opt/airflow/data/raw/"
+CSV_PATH = "/opt/airflow/data/raw/games.csv"
 TMP_PATH_RAW = "/opt/airflow/data/tmp_raw.csv"
 TMP_PATH_TRANSFORMED = "/opt/airflow/data/tmp_transformed.csv"
 COLUMNS_TO_SELECT = [
@@ -24,6 +27,8 @@ def etl_steam_games():
 
     @task
     def extract() -> str:
+        with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref:
+            zip_ref.extractall('/opt/airflow/data/raw/')
         df = pd.read_csv(CSV_PATH)
         df.to_csv(TMP_PATH_RAW, index=False)
         return TMP_PATH_RAW
@@ -31,7 +36,7 @@ def etl_steam_games():
     @task
     def transform(input_path: str) -> str:
         df = pd.read_csv(input_path)
-        temp_path = "/opt/airflow/data/tmp_transformed.csv"
+        os.remove(TMP_PATH_RAW)
         df = df[COLUMNS_TO_SELECT]
         df["Positive Percentage"] = df["Positive"] / (df["Positive"] + df["Negative"]) * 100
         df.to_csv(TMP_PATH_TRANSFORMED, index=False)
@@ -41,6 +46,7 @@ def etl_steam_games():
     def load(transformed_path: str):
         engine = create_engine(os.getenv("DATABASE_URL", "postgresql://user:password@postgres:5432/steamdb"))
         df = pd.read_csv(transformed_path)
+        os.remove(TMP_PATH_TRANSFORMED)
         df.to_sql("steam_games", con=engine, if_exists="replace", index=False)
 
     raw_csv = extract()
