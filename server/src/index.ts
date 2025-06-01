40ba936d4e93d34ed68d8b883f8829d311a39093
Stepan Manaenko <u4zarus@gmail.com>
2025-06-01 17:37:15 +0200

    Add filtering and pages. Fix auto reload in yaml

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,17 +13,71 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/games", async (req, res) => {
+    const page = parseInt(req.query.page as string) || 1;
+    const genre = req.query.genre as string | undefined;
+    const limit = 10;
+    const offset = (page - 1) * limit;
+
     try {
-        const result = await pool.query(
-            'SELECT "Name" FROM steam_games LIMIT 10;'
-        );
-        const gameNames = result.rows.map((row) => row.Name);
-        res.json(gameNames);
+        let query = `
+            SELECT 
+                "Name", 
+                "Header image", 
+                "Genres", 
+                "User score", 
+                "About the game" 
+            FROM steam_games
+        `;
+        const values: any[] = [];
+
+        if (genre) {
+            query += ` WHERE "Genres" ILIKE $1`;
+            values.push(`%${genre}%`);
+        }
+
+        query += ` ORDER BY "User score" DESC LIMIT $${
+            values.length + 1
+        } OFFSET $${values.length + 2}`;
+        values.push(limit, offset);
+
+        const result = await pool.query(query, values);
+
+        console.log(result.rows);
+
+        const games = result.rows.map((row) => ({
+            name: row.Name,
+            imageSrc: row["Header image"],
+            genres: row.Genres
+                ? row.Genres.split(",").map((g: string) => g.trim())
+                : [],
+            userRating: row["User score"] ? Math.round(row["User score"]) : 0,
+            description: row["About the game"] || "No description available.",
+            released: true,
+            similarGames: [],
+        }));
+
+        res.json(games);
     } catch (error) {
         console.error("Error fetching games:", error);
         res.status(500).json({ error: "Failed to fetch games" });
     }
 });
+
+// app.post("/api/predict", async (req, res) => {
+//     const { genres, categories, about, age } = req.body;
+//     try {
+//         const result = await pool.query("SELECT predict($1, $2, $3, $4)", [
+//             genres,
+//             categories,
+//             about,
+//             age,
+//         ]);
+//         res.json({ prediction: result.rows[0].predict });
+//     } catch (error) {
+//         console.error("Error making prediction:", error);
+//         res.status(500).json({ error: "Failed to make prediction" });
+//     }
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is up: http://localhost:${PORT}`);
